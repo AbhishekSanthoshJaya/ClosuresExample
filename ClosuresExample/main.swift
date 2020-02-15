43fa1ec3963d6e5fb45c5f9071e29cdbf4ef9ba9
AbhishekSanthoshJaya <abhisheksj35@gmail.com>
2020-02-14 21:02:53 -0500

    Day7 stuff

--- ClosuresExample/main.swift
+++ ClosuresExample/main.swift
@@ -8,5 +8,72 @@
 
 import Foundation
 
-print("Hello, World!")
+func add (a: Int, b: Int)
+{
+    print(a+b)
+}
 
+add(a: 10, b:20)
+
+var f : (Int, Int) -> Void
+f = add
+
+f(10,40)
+
+func test(a: Int, b: (Int, Int) -> Void)
+{
+    b(a, 10)
+}
+
+test(a:20, b:f)
+
+var country = ["Canada", "Abc", "India", "Bhutan", "Nepal", "China", "USA", "Mexico"]
+
+func mySort(s1: String, s2: String) -> Bool{
+    return s1 > s2
+}
+
+print(mySort(s1: "abc", s2: "aac"))
+
+print(country)
+//country.sort()
+//let c = country.sorted()
+//print(c)
+
+//country.sort(by: mySort(s1:s2:))
+print(country)
+country.sort(by:
+    {
+        (s1: String, s2: String) -> Bool in
+        return s1 > s2
+    })
+
+country.sort(by:
+{
+    (s1, s2) -> Bool in
+    return s1 > s2
+})
+
+country.sort(by:
+{
+    s1, s2 -> Bool in
+    return s1 > s2
+})
+
+country.sort(by:
+{
+    return $0 > $1
+})
+
+country.sort(by: >)
+country.sort(by: <)
+print(country)
+
+let s = country.map {(str) -> String in
+    if str.count == 5
+    {
+        return str
+    }
+    return String()
+}
+print(s)
